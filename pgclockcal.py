35182e14be550669c8c8d3c0df3be565e464083d
Seukwon Kang <kasworld@gmail.com>
2021-07-08 11:30:05 +0900

    add weather from naver

--- pgclockcal.py
+++ pgclockcal.py
@@ -6,6 +6,23 @@ import sys
 import time
 import datetime
 
+import requests
+from bs4 import BeautifulSoup
+
+
+def getNaverWeather():
+    source = requests.get('https://www.naver.com/')
+    soup = BeautifulSoup(source.content, "html.parser")
+
+    group_weather = soup.find('div', {'class': 'group_weather'})
+    current_box = group_weather.find('div', {'class': 'current_box'})
+    current = current_box.find('strong', {'class': 'current'}).text
+    current_state = current_box.find('strong', {'class': 'state'}).text
+    location = group_weather.find('span', {'class': 'location'}).text
+    weatherUpdate = datetime.datetime.now()
+    return current, current_state, location, weatherUpdate
+
+
 success, fail = pygame.init()
 
 # screen = pygame.display.set_mode(flags=pygame.FULLSCREEN) # not work on linux
@@ -15,7 +32,7 @@ screenW, screenH = screen.get_size()
 dayW = screenW/14
 dayH = screenH/12
 calendarBaseY = screenH - dayH*7
-calendarBaseX = screenW - dayW*7
+calendarBaseX = screenW - dayW*7 - dayW/4
 
 bigFt = pygame.font.SysFont(None, int(screenH/1.7))
 midFt = pygame.font.SysFont(None, int(screenH/6.6))
@@ -23,19 +40,15 @@ smallFt = pygame.font.SysFont(None, int(screenH/11))
 
 tick = pygame.time.Clock()
 
+current, current_state, location, weatherUpdate = getNaverWeather()
+
 
 def calcCenter(suf, refW=screenW):
     sufW = suf.get_width()
     return (refW-sufW)/2
 
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-    tick.tick(60)
-
+def drawClockCal():
     screen.fill((0, 0, 0))
 
     # for x in range(0,screenW,1):
@@ -55,6 +68,11 @@ while 1:
     dateText = "{0:%Y-%m-%d %a}".format(datetime.datetime.now())
     txtSuf = midFt.render(dateText, False, (255, 255, 255))
     screen.blit(txtSuf, (calcCenter(txtSuf, screenW/2), screenH/2))
+
+    # draw weather
+    weatherText = current
+    txtSuf = midFt.render(weatherText, False, (255, 255, 255))
+    screen.blit(txtSuf, (calcCenter(txtSuf, screenW/2), screenH/2+dayH*2))
 
     # draw 6 week calendar
     today = datetime.datetime.now()
@@ -88,3 +106,16 @@ while 1:
             dayIndex += datetime.timedelta(days=1)
 
     pygame.display.flip()
+
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            sys.exit()
+
+    tick.tick(2)
+    if datetime.datetime.now() - weatherUpdate > datetime.timedelta(minutes=10):
+        current, current_state, location, weatherUpdate = getNaverWeather()
+    drawClockCal()
